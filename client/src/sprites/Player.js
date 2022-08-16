90abd204ef3359cfb48d71b26ba55e9c7bd5f6bf
alexisprovost <provost21j@gmail.com>
2022-08-15 21:11:53 -0400

    removed hit floor

--- client/src/sprites/Player.js
+++ client/src/sprites/Player.js
@@ -7,39 +7,41 @@ class Player {
 		this.y = -150;
 		this.speed = 10;
 		this.gravity = 1;
-		this.velocity = -0.5;
+		this.velocity = -0.25;
 		this.alive = true;
+		this.onGround = false;
+
+		this.node.addEventListener("click", function () {
+			this.style.backgroundColor =
+				"rgb(" + Math.random() * 255 + ", " + Math.random() * 255 + ", " + Math.random() * 255 + ")";
+		});
 
 		parent.append(this.node);
 	}
 
 	tick(keys) {
+		//Woosh gravity
 		if (keys[37] || keys[65]) {
+			console.log("left");
 			this.x -= this.speed;
 		}
 		if (keys[38] || keys[87]) {
-			if (Math.ceil(this.y) == -54) {
-				this.gravity = 1;
-				this.y += this.speed;
-			}
+			console.log("jump");
 			this.y -= this.speed;
 		}
 		if (keys[39] || keys[68]) {
+			console.log("right");
 			this.x += this.speed;
 		}
 		if (keys[40] || keys[83]) {
-			if (this.y < -57) {
-				this.y += this.speed;
-			}
+			console.log("down");
+			this.y += this.speed;
 		}
 
-		//Woosh gravity
+		this.gravity -= this.velocity;
+		this.y += this.gravity;
 
-		if (this.y < -57 || this.x < -57 || this.x > 430) {
-			this.gravity -= this.velocity;
-			this.y += this.gravity;
-		}
-
+		// Kill player if they fall off the screen
 		if (this.y >= document.body.scrollHeight) {
 			this.alive = false;
 			this.node.remove();
@@ -48,7 +50,7 @@ class Player {
 		this.node.style.left = this.x + "px";
 		this.node.style.top = this.y + "px";
 
-		console.log(this.x, this.y);
+		//console.log(this.x, this.y);
 
 		return this.alive;
 	}
