de3396a0241f7aa1cd7e879b0f151df9022ba1c4
alexisprovost <provost21j@gmail.com>
2022-08-16 15:13:50 -0400

    Clouds

--- client/src/page-index.js
+++ client/src/page-index.js
@@ -1,10 +1,24 @@
 import { signin } from "./chat-api";
 import Player from "./sprites/Player";
-let players, keys;
+import BackgroundLayer from "./sprites/BackgroundLayer";
+let players, bgLayers, keys;
+
+const setupBackground = () => {
+	bgLayers.push(new BackgroundLayer(document.body,"img/01/sky.png", 0.25));
+	bgLayers.push(new BackgroundLayer(document.body,"img/01/clouds_1.png", -0.25, true));
+	bgLayers.push(new BackgroundLayer(document.body,"img/01/clouds_2.png", -0.5, true));
+	bgLayers.push(new BackgroundLayer(document.body,"img/01/rocks_1.png", 0));
+	bgLayers.push(new BackgroundLayer(document.body,"img/01/rocks_2.png", 0));
+	bgLayers.push(new BackgroundLayer(document.body,"img/01/clouds_3.png", -0.75, true));
+	bgLayers.push(new BackgroundLayer(document.body,"img/01/clouds_4.png", -1, true));
+}
 
 window.addEventListener("load", () => {
 	players = [];
 	keys = [];
+	bgLayers = [];
+
+	setupBackground();
 
 	const playground = document.getElementById("playground");
 
@@ -49,6 +63,18 @@ const tick = function () {
 
 				players.push(player);
 			}, 1000);
+		}
+	}
+
+	for (let i = 0; i < bgLayers.length; i++) {
+		const element = bgLayers[i];
+
+		if(!element.tick()) {
+
+			bgLayers.push(new BackgroundLayer(document.body, element.getImg(), element.getSpeed()));
+
+			bgLayers.splice(i, 1);
+			i--;
 		}
 	}
 
---
+++ client/src/sprites/BackgroundLayer.js
@@ -0,0 +1,53 @@
+class BackgroundLayer {
+	constructor(parent, img, speed, fadeIn = false) {
+		this.parent = parent;
+		this.img = img;
+		this.alive = true;
+		this.node = document.createElement("div");
+		this.node.classList.add("background");
+		if (fadeIn) {
+			this.node.classList.add("fadeIn");
+		}
+
+		this.x = 0;
+		this.y = 0;
+		this.speed = speed;
+
+		this.node.style.opacity = 1;
+
+		this.node.style.backgroundImage = "url(" + img + ")";
+
+		parent.append(this.node);
+	}
+
+	getParent = () => {
+		return this.parent;
+	};
+
+	getImg = () => {
+		return this.img;
+	};
+
+	getSpeed = () => {
+		return this.speed;
+	};
+
+	tick() {
+		if (this.x >= screen.width || this.x <= -screen.width) {
+			this.node.style.opacity -= 0.01;
+
+			if (this.node.style.opacity <= 0) {
+				this.node.remove();
+				this.alive = false;
+			}
+		}
+
+		this.x -= this.speed;
+
+		this.node.style.backgroundPositionX = this.x + "px";
+
+		return this.alive;
+	}
+}
+
+export default BackgroundLayer;
